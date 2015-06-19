7bc9ce8ee34c02974e0a99153dc937b0086fd811
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-19 08:59:31 +0000

    A CSS Bug  removed
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-social-buttons/trunk@1184051 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -86,24 +86,25 @@ function va_wc_sh_form_code() {
 
 function va_wc_sh_form_short_code() {
     global $post;
-    $social_val = '';
-    
-        if (get_option('va_share_like_fb') == 'true') {
-            $social_val.='<span class="fb-share-button" data-href="' . get_permalink($post->ID) . '" data-layout="button_count"></span>';
-        }
-        if (get_option('va_share_like_gp') == 'true') {
-            $social_val.='<span class="g-plus" data-action="share" data-annotation="bubble" data-href="' . get_permalink($post->ID) . '"></span>';
-        }
-        if (get_option('va_share_like_li') == 'true') {
-            $social_val.='<span><script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
-                          <script type="IN/Share" data-counter="right"></script></span>';
-        }
-        if (get_option('va_share_like_tw') == 'true') {
-            $social_val.='<span><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a></span></div>';
-        }
+   $social_val = '<div class="social-media-icons">';
+    if (get_option('va_share_like_fb') == 'true') {
+        $social_val.='<div class="facebook-button"><div class="fb-share-button" data-href="' . get_permalink($post->ID) . '" data-layout="button_count"></div></div>';
+    }
+    if (get_option('va_share_like_gp') == 'true') {
+        $social_val.='<div class="gplus-button"><div  class="g-plus" data-action="share" data-annotation="bubble" data-href="' . get_permalink($post->ID) . '"></div></div>';
+    }
+    if (get_option('va_share_like_li') == 'true') {
+        $social_val.='<script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+                          <div class="linkedin-button"><script type="IN/Share" data-url="' . get_permalink($post->ID) . '" data-counter="right"></script></div>';
+    }
+    if (get_option('va_share_like_tw') == 'true') {
+        $social_val.='<div class="twitter-button"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a></div>';
+    }
    
+    $social_val .= '</div>';
+
     
-    echo $social_val;
+   return $social_val;
 }
 
 
@@ -165,7 +166,8 @@ function va_wc_sh_social_facebook_init() {
             <div id="setting-error-settings_updated" class="updated settings-error"> 
                 <p><strong>Settings saved.</strong></p></div>
         <?php } ?>
-        <div class="container_div" >
+        <div class="inner_wrap">
+        <div class="left container_div" >
             <form name="social" method="post" >   
                 <table class="share_form_table" >
                     <tr><th>
@@ -218,6 +220,52 @@ function va_wc_sh_social_facebook_init() {
                 </div>
             </form>
         </div>
+        
+         <div class="right">
+	<center>
+<div class="bottom">
+		    <h3 id="download-comments-wvpd" class="title"><?php _e( 'Download Free Plugins', 'wvpd' ); ?></h3>
+		     
+     <div id="downloadtbl-comments-wvpd" class="togglediv">  
+	<h3 class="company">
+<p> Vivacity InfoTech Pvt. Ltd. is an ISO 9001:2008 Certified Company is a Global IT Services company with expertise in outsourced product development and custom software development with focusing on software development, IT consulting, customized development.We have 200+ satisfied clients worldwide.</p>	
+<?php _e( 'Our Top 5 Latest Plugins', 'wvpd' ); ?>:
+</h3>
+<ul class="">
+<li><a target="_blank" href="https://wordpress.org/plugins/woocommerce-social-buttons/">Woocommerce Social Buttons</a></li>
+<li><a target="_blank" href="https://wordpress.org/plugins/vi-random-posts-widget/">Vi Random Post Widget</a></li>
+<li><a target="_blank" href="http://wordpress.org/plugins/wp-infinite-scroll-posts/">WP EasyScroll Posts</a></li>
+<li><a target="_blank" href="https://wordpress.org/plugins/buddypress-social-icons/">BuddyPress Social Icons</a></li>
+<li><a target="_blank" href="http://wordpress.org/plugins/wp-fb-share-like-button/">WP Facebook Like Button</a></li>
+</ul>
+  </div> 
+</div>		
+<div class="bottom">
+		    <h3 id="donatehere-comments-wvpd" class="title"><?php _e( 'Donate Here', 'wvpd' ); ?></h3>
+     <div id="donateheretbl-comments-wvpd" class="togglediv">  
+     <p><?php _e( 'If you want to donate , please click on below image.', 'wvpd' ); ?></p>
+	<a href="http://bit.ly/1icl56K" target="_blank"><img class="donate" src="<?php echo plugins_url( 'assets/paypal.gif' , __FILE__ ); ?>" width="150" height="50" title="<?php _e( 'Donate Here', 'wvpd' ); ?>"></a>		
+  </div> 
+</div>	
+<div class="bottom">
+		    <h3 id="donatehere-comments-wvpd" class="title"><?php _e( 'Woocommerce Frontend Plugin', 'wvpd' ); ?></h3>
+     <div id="donateheretbl-comments-wvpd" class="togglediv">  
+     <p><?php _e( 'If you want to purchase , please click on below image.', 'wvpd' ); ?></p>
+	<a href="http://bit.ly/1HZGRBg" target="_blank"><img class="donate" src="<?php echo plugins_url( 'assets/woo_frontend_banner.png' , __FILE__ ); ?>" width="336" height="280" title="<?php _e( 'Donate Here', 'wvpd' ); ?>"></a>		
+  </div> 
+</div>
+<div class="bottom">
+		    <h3 id="donatehere-comments-wvpd" class="title"><?php _e( 'Blue Frog Template', 'wvpd' ); ?></h3>
+     <div id="donateheretbl-comments-wvpd" class="togglediv">  
+     <p><?php _e( 'If you want to purchase , please click on below image.', 'wvpd' ); ?></p>
+	<a href="http://bit.ly/1Gwp4Vv" target="_blank"><img class="donate" src="<?php echo plugins_url( 'assets/blue_frog_banner.png' , __FILE__ ); ?>" width="336" height="280" title="<?php _e( 'Donate Here', 'wvpd' ); ?>"></a>		
+  </div> 
+</div>
+	</center>
+
+        
+        </div>
+        </div>
     </div>
     <?php
 }
@@ -227,4 +275,11 @@ function va_wc_sh_my_enqueue() {
 }
 
 add_action('wp_print_scripts', 'va_wc_sh_my_enqueue');
+
+function woocommerce_social_scripts_frontend() {
+    wp_register_style('foo-styles', plugin_dir_url(__FILE__) . 'button.css');
+    wp_enqueue_style('foo-styles');
+}
+
+add_action('wp_enqueue_scripts', 'woocommerce_social_scripts_frontend');
 ?>
